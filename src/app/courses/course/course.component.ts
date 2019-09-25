58f70d3da140f40e54624797b46a82dfdbb28b95
JHades <jhades.dev@gmail.com>
2019-09-25 15:08:03 +0200

    NestJs Course

--- src/app/courses/course/course.component.ts
+++ src/app/courses/course/course.component.ts
@@ -1,10 +1,11 @@
-import {AfterViewInit, Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, QueryList, ViewChild, ViewChildren} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Course} from '../../../../shared/course';
 import {Observable} from 'rxjs';
 import {Lesson} from '../../../../shared/lesson';
-import {concatMap, delay, filter, first, map, shareReplay, tap, withLatestFrom} from 'rxjs/operators';
+import {concatMap, delay, filter, first, map, shareReplay, switchMap, tap, withLatestFrom} from 'rxjs/operators';
 import {CoursesHttpService} from '../services/courses-http.service';
+import {MatPaginator, PageEvent} from '@angular/material';
 
 
 @Component({
@@ -20,7 +21,13 @@ export class CourseComponent implements OnInit {
 
   displayedColumns = ['seqNo', 'description', 'duration'];
 
-  nextPage = 0;
+  currentPage = 0;
+
+  @ViewChildren(MatPaginator)
+  paginators: QueryList<MatPaginator>;
+
+  @ViewChild(MatPaginator, {static:false})
+  paginator: MatPaginator;
 
   constructor(
     private coursesService: CoursesHttpService,
@@ -34,16 +41,30 @@ export class CourseComponent implements OnInit {
 
     this.course$ = this.coursesService.findCourseByUrl(courseUrl);
 
-    this.lessons$ = this.course$.pipe(
-      concatMap(course => this.coursesService.findLessons(course._id)),
-      tap(console.log)
-    );
+    this.loadLessonsPage();
 
   }
 
+  ngAfterViewInit() {
 
-  loadLessonsPage(course: Course) {
+    this.paginators.changes
+      .pipe(
+        filter(paginators => paginators.length > 0),
+        switchMap(paginators => paginators.first.page)
+      )
+      .subscribe(
+        (page:PageEvent) => {
+        this.currentPage = page.pageIndex;
+        this.loadLessonsPage();
+      }
+    );
+  }
 
+  loadLessonsPage() {
+    console.log("current page", this.currentPage);
+    this.lessons$ = this.course$.pipe(
+      concatMap(course => this.coursesService.findLessons(course._id, this.currentPage, 3)),
+    );
   }
 
 }
