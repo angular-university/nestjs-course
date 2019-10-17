14618c5307ef78aae0d47dd76324fb14aa5e5a02
Your Name <you@example.com>
2019-10-17 15:30:48 +0200

    NestJs Course

--- rest-api/src/main.ts
+++ rest-api/src/main.ts
@@ -3,7 +3,9 @@ import {AppModule} from './app.module';
 import {HttpExceptionFilter} from './filters/http.filter';
 import {FallbackExceptionFilter} from './filters/fallback.filter';
 import * as mongoose from 'mongoose';
-import {ValidationPipe} from '@nestjs/common';
+import {ValidationError, ValidationPipe} from '@nestjs/common';
+import {ValidationFilter} from './filters/validation.filter';
+import {ValidationException} from './filters/validation.exception';
 
 mongoose.set('useFindAndModify', false);
 
@@ -12,12 +14,24 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
     app.setGlobalPrefix("api");
+
     app.useGlobalFilters(
         new FallbackExceptionFilter(),
-        new HttpExceptionFilter());
+        new HttpExceptionFilter(),
+        new ValidationFilter()
+    );
 
     app.useGlobalPipes(new ValidationPipe({
-        skipMissingProperties:true
+        skipMissingProperties:true,
+        exceptionFactory: (errors: ValidationError[]) => {
+
+            const messages = errors.map(
+                error=> `${error.property} has wrong value ${error.value},
+                ${Object.values(error.constraints).join(', ')} `
+            );
+
+            return new ValidationException(messages);
+        }
     }));
 
     await app.listen(9000);
