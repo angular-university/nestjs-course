6215c85489914c652ded0c8aaf6dc786077471b8
Your Name <you@example.com>
2019-10-31 12:45:01 +0100

    NestJs Course

--- rest-api/src/courses/courses.module.ts
+++ rest-api/src/courses/courses.module.ts
@@ -3,14 +3,14 @@ import {CoursesController} from './controllers/courses.controller';
 import {MongooseModule} from '@nestjs/mongoose';
 import {CoursesSchema} from './schemas/courses.schema';
 import {CoursesRepository} from './repositories/courses.repository';
+import {LessonsSchema} from './schemas/lessons.schema';
 
 
 @Module({
     imports: [
         MongooseModule.forFeature([
-            {
-                name: "Course", schema: CoursesSchema
-            }
+            {name: "Course", schema: CoursesSchema},
+            {name: "Lesson", schema: LessonsSchema}
         ])
     ],
     controllers: [
