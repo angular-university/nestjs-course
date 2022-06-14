ca4346e2c1505d8d135d63ba47ed49fd54d4ad7d
jhades <jhades.dev@gmail.com>
2022-06-14 15:49:40 +0200

    Upgrade to Angular 14

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -39,41 +39,40 @@ export const coursesRoutes: Routes = [
 
 
 @NgModule({
-  imports: [
-    CommonModule,
-    MatButtonModule,
-    MatIconModule,
-    MatCardModule,
-    MatTabsModule,
-    MatInputModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatProgressSpinnerModule,
-    MatSlideToggleModule,
-    MatDialogModule,
-    MatSelectModule,
-    MatDatepickerModule,
-    MatMomentDateModule,
-    ReactiveFormsModule,
-    RouterModule.forChild(coursesRoutes)
-  ],
-  declarations: [
-    HomeComponent,
-    CoursesCardListComponent,
-    EditCourseDialogComponent,
-    CourseComponent
-  ],
-  exports: [
-    HomeComponent,
-    CoursesCardListComponent,
-    EditCourseDialogComponent,
-    CourseComponent
-  ],
-  entryComponents: [EditCourseDialogComponent],
-  providers: [
-    CoursesHttpService
-  ]
+    imports: [
+        CommonModule,
+        MatButtonModule,
+        MatIconModule,
+        MatCardModule,
+        MatTabsModule,
+        MatInputModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatProgressSpinnerModule,
+        MatSlideToggleModule,
+        MatDialogModule,
+        MatSelectModule,
+        MatDatepickerModule,
+        MatMomentDateModule,
+        ReactiveFormsModule,
+        RouterModule.forChild(coursesRoutes)
+    ],
+    declarations: [
+        HomeComponent,
+        CoursesCardListComponent,
+        EditCourseDialogComponent,
+        CourseComponent
+    ],
+    exports: [
+        HomeComponent,
+        CoursesCardListComponent,
+        EditCourseDialogComponent,
+        CourseComponent
+    ],
+    providers: [
+        CoursesHttpService
+    ]
 })
 export class CoursesModule {
 
